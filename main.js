2559723d279c5964d6a409efcd06863234e0be36
VLAMMORA <101669631+VLAMMORA@users.noreply.github.com>
2023-11-07 17:39:07 -0600

    Add files via upload
    Variables, Arreglos y Objetos

---
+++ main.js
@@ -0,0 +1,63 @@
+// console.log("Funcionando")
+
+
+// var nombre = "Mi nombre es Marcelo "
+
+// console.log(nombre)
+
+
+// // alert(nombre)
+
+
+// //Function
+// function Sumar() {
+    
+//     alert("Funcionando")
+
+    
+// }
+
+// function Holaaa() {
+//     console.log("Me diste click")
+// }
+
+//Javascript 
+// 1-Tipos de datos
+// 2-Arreglos 
+// 3-Objetos 
+// 4-Ciclos
+// 5-Condicionales
+// 6-Funciones 
+
+
+//tipos de datos
+
+var mascota = "gato" // String texto
+
+var edad = 34 //integer numerica
+
+console.log(mascota)
+
+console.log(edad)
+
+//array arreglo 
+
+
+// Arrays [] para representar datos Multiples 
+var mascotas= ["Gato","perro","conejo","ballena",25]  //array arreglo muchos 
+
+console.log(mascotas[4])
+//Para Mostrar la informacion de un array, se coloca el nombre del array y [posicion] se cuenta a partir de 0. mascotas[4]
+
+// Objeto definir caracteristicas que algo que represente la vida real {} llaves , corchetes 
+
+var Celular ={
+    marca: "Samsung",
+    color: "Negro",
+    cantidad_camaras: 2,
+    cantidad_pantalla:1
+
+}
+//Para Mostrar la informacion de un objeto, se coloca el nombre del objeto y .Celular.marca
+console.log(Celular.marca)  
+console.log(Celular.color)
